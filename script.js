bfc4b992a8ed6366f44d173b82493a5d88936f74
Yash Repal <yashrepal0508@gmail.com>
2023-08-02 09:13:01 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// header toggle
+
+let MenuBtn = document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click', function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
